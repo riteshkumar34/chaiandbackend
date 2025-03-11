9f744ecf9d43fcfacbdc3ef76a34282e61c7c452
Ritesh Kumar <riteshandshan@gmail.com>
2025-03-11 12:10:21 +0530

    user models

--- src/index.js
+++ src/index.js
@@ -1,15 +1,25 @@
+//require('dotenv).config({path:'./env'})
 
 import mongoose from "mongoose";
-import {DB_NAME} from "./constants";
-import connectDB from "./db";
+import {DB_NAME} from  "./constants.js";
+import dotenv from "dotenv";
+import connectDB from "../src/db/index.js";
 
+// dotenv.config({
+//     path: './env'
+// })
+
+dotenv.config({path:"./env"});
 
 connectDB()
-
-
-
-
-
+.then(() => {
+    application.listen(process.env.PORT || 8000,()=>{
+        console.log(`Server is running at port: ${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("MONGO db connection failed!!!",err);
+})
 
 
 
